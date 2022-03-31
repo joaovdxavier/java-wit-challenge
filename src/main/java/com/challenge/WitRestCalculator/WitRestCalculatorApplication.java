077958ce703763f4a52fe68c9ccc745a412dc97d
João Xavier <joao.xavier@quark.tec.br>
2022-03-31 12:58:44 -0300

    [João] - adicionando projeto inicial

---
+++ src/main/java/com/challenge/WitRestCalculator/WitRestCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.WitRestCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitRestCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitRestCalculatorApplication.class, args);
+	}
+
+}
